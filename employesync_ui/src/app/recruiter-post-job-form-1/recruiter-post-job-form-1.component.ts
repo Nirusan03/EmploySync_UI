9c4db03e1625cfa9bf25cd28dbf6f2b0ce12b0d6
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-11 13:41:29 +0530

    [T11] Posting job form 1 content added

--- employesync_ui/src/app/recruiter-post-job-form-1/recruiter-post-job-form-1.component.ts
+++ employesync_ui/src/app/recruiter-post-job-form-1/recruiter-post-job-form-1.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RecruiterPostJobService } from '../services/recruiter-post-job.service';
 
 @Component({
   selector: 'app-recruiter-post-job-form-1',
-  standalone: true,
-  imports: [],
-  templateUrl: './recruiter-post-job-form-1.component.html',
-  styleUrl: './recruiter-post-job-form-1.component.css'
+  templateUrl: './recruiter-post-job-form_1.component.html',
+  styleUrls: ['./recruiter-post-job-form_1.component.css']
 })
 export class RecruiterPostJobForm1Component {
+  jobTitle = '';
+  position = 'Remote';
+  jobType = 'Full-time';
+  minEducation = '';
 
+  constructor(private router: Router, private jobService: RecruiterPostJobService) {
+    const savedData = this.jobService.getJobData();
+    this.jobTitle = savedData.jobTitle;
+    this.position = savedData.position;
+    this.jobType = savedData.jobType;
+    this.minEducation = savedData.minEducation;
+  }
+
+  nextStep() {
+    this.jobService.updateJobData({ jobTitle: this.jobTitle, position: this.position, jobType: this.jobType, minEducation: this.minEducation });
+    this.router.navigate(['/post-job/step-2']);
+  }
 }
