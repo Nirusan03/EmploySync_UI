cf0a7c2cceb09b9363bc1fc4332b4c19dbad7962
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-11 13:48:49 +0530

    [T11-2] Recruiter 2nd form componet defined

--- employesync_ui/src/app/recruiter-post-job-form-2/recruiter-post-job-form-2.component.ts
+++ employesync_ui/src/app/recruiter-post-job-form-2/recruiter-post-job-form-2.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RecruiterPostJobService } from '../services/recruiter-post-job.service';
 
 @Component({
   selector: 'app-recruiter-post-job-form-2',
-  standalone: true,
-  imports: [],
-  templateUrl: './recruiter-post-job-form-2.component.html',
-  styleUrl: './recruiter-post-job-form-2.component.css'
+  templateUrl: './recruiter-post-job-form_2.component.html',
+  styleUrls: ['./recruiter-post-job-form_2.component.css']
 })
 export class RecruiterPostJobForm2Component {
+  salaryType = 'per Hour';
+  salaryMin = 50;
+  salaryMax = 1000;
+  skills: string[] = [];
+  skillInput = '';
 
+  constructor(private router: Router, private jobService: RecruiterPostJobService) {
+    const savedData = this.jobService.getJobData();
+    this.salaryType = savedData.salaryType;
+    this.salaryMin = savedData.salaryMin;
+    this.salaryMax = savedData.salaryMax;
+    this.skills = savedData.skills;
+  }
+
+  addSkill() {
+    if (this.skillInput.trim()) {
+      this.skills.push(this.skillInput.trim());
+      this.skillInput = '';
+    }
+  }
+
+  removeSkill(index: number) {
+    this.skills.splice(index, 1);
+  }
+
+  nextStep() {
+    this.jobService.updateJobData({ salaryType: this.salaryType, salaryMin: this.salaryMin, salaryMax: this.salaryMax, skills: this.skills });
+    this.router.navigate(['/post-job/step-3']);
+  }
+
+  previousStep() {
+    this.router.navigate(['/post-job/step-1']);
+  }
 }
