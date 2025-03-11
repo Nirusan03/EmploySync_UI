eb2179e175e606a3fb2e8630f6cf32d4ffc0c18b
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-11 13:54:52 +0530

    [T11-4] Recruiter 4th posting job form componet defined

--- employesync_ui/src/app/recruiter-post-job-form-4/recruiter-post-job-form-4.component.ts
+++ employesync_ui/src/app/recruiter-post-job-form-4/recruiter-post-job-form-4.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RecruiterPostJobService } from '../services/recruiter-post-job.service';
 
 @Component({
   selector: 'app-recruiter-post-job-form-4',
-  standalone: true,
-  imports: [],
-  templateUrl: './recruiter-post-job-form-4.component.html',
-  styleUrl: './recruiter-post-job-form-4.component.css'
+  templateUrl: './recruiter-post-job-form_4.component.html',
+  styleUrls: ['./recruiter-post-job-form_4.component.css']
 })
 export class RecruiterPostJobForm4Component {
+  jobDescription = '';
 
+  constructor(private router: Router, private jobService: RecruiterPostJobService) {
+    const savedData = this.jobService.getJobData();
+    this.jobDescription = savedData.jobDescription;
+  }
+
+  postJob() {
+    this.jobService.updateJobData({ jobDescription: this.jobDescription });
+
+    // Simulate API Call
+    console.log('Job Posted:', this.jobService.getJobData());
+    alert('Job successfully posted!');
+
+    // Navigate back to job listing or dashboard
+    this.router.navigate(['/jobs']);
+  }
+
+  previousStep() {
+    this.router.navigate(['/post-job/step-3']);
+  }
 }
