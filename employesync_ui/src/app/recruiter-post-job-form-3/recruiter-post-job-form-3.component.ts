0e596b913c04ce0a6905867d76e656e1363ca9dc
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-11 13:51:48 +0530

    [T11-3] Recruiter 2nd posting job form componet defined

--- employesync_ui/src/app/recruiter-post-job-form-3/recruiter-post-job-form-3.component.ts
+++ employesync_ui/src/app/recruiter-post-job-form-3/recruiter-post-job-form-3.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RecruiterPostJobService } from '../services/recruiter-post-job.service';
 
 @Component({
   selector: 'app-recruiter-post-job-form-3',
-  standalone: true,
-  imports: [],
-  templateUrl: './recruiter-post-job-form-3.component.html',
-  styleUrl: './recruiter-post-job-form-3.component.css'
+  templateUrl: './recruiter-post-job-form_3.component.html',
+  styleUrls: ['./recruiter-post-job-form_3.component.css']
 })
 export class RecruiterPostJobForm3Component {
+  experienceMin = 1;
+  experienceMax = 5;
+  screeningQuestions: string[] = [];
 
+  constructor(private router: Router, private jobService: RecruiterPostJobService) {
+    const savedData = this.jobService.getJobData();
+    this.experienceMin = savedData.experienceMin;
+    this.experienceMax = savedData.experienceMax;
+    this.screeningQuestions = savedData.screeningQuestions;
+  }
+
+  addQuestion() {
+    this.screeningQuestions.push('');
+  }
+
+  removeQuestion(index: number) {
+    this.screeningQuestions.splice(index, 1);
+  }
+
+  nextStep() {
+    this.jobService.updateJobData({ experienceMin: this.experienceMin, experienceMax: this.experienceMax, screeningQuestions: this.screeningQuestions });
+    this.router.navigate(['/post-job/step-4']);
+  }
+
+  previousStep() {
+    this.router.navigate(['/post-job/step-2']);
+  }
 }
