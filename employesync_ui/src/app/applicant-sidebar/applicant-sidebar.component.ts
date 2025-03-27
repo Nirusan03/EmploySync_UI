857e68a7193ae6dc8e1bd379c39e98ef7ccec1ad
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-27 19:02:58 +0530

    [1] Applicant side bar added

---
+++ employesync_ui/src/app/applicant-sidebar/applicant-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applicant-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './applicant-sidebar.component.html',
+  styleUrl: './applicant-sidebar.component.css'
+})
+export class ApplicantSidebarComponent {
+
+}
