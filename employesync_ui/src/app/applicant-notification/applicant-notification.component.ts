30fba23485c55f4491a66e503ac6c2494fb4c6ce
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-28 06:00:29 +0530

    [T27] Notification structure added

--- employesync_ui/src/app/applicant-notification/applicant-notification.component.ts
+++ employesync_ui/src/app/applicant-notification/applicant-notification.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-applicant-notification',
   standalone: true,
-  imports: [],
   templateUrl: './applicant-notification.component.html',
-  styleUrl: './applicant-notification.component.css'
+  styleUrls: ['./applicant-notification.component.css'],
+  imports: [CommonModule, MatIconModule, MatButtonModule],
 })
 export class ApplicantNotificationComponent {
+  shortlistedJobs: string[];
 
+  constructor(
+    public dialogRef: MatDialogRef<ApplicantNotificationComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { jobs: string[] }
+  ) {
+    this.shortlistedJobs = data.jobs || [];
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
 }
