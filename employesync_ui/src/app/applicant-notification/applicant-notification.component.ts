812fe6b2d223b08b8ef6876c6394c704a3291962
Nirusan Hariharan <hariharan.20200094@iit.ac.lk>
2025-03-28 05:59:02 +0530

    [T26] Notification component added

---
+++ employesync_ui/src/app/applicant-notification/applicant-notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applicant-notification',
+  standalone: true,
+  imports: [],
+  templateUrl: './applicant-notification.component.html',
+  styleUrl: './applicant-notification.component.css'
+})
+export class ApplicantNotificationComponent {
+
+}
